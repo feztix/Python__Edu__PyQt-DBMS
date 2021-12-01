5b34b526c8d707c6a1012bf041af4c453553415c
feztix <pan0v.s.s@yandex.ru>
2021-12-01 09:58:41 +0300

    Stage 2.3/3: Complete Task 2

--- BinaryTree.py
+++ BinaryTree.py
@@ -16,29 +16,13 @@ import numpy
 import collections
 import json
 
+
 class TreeNode(object):
     def __init__(self, x):
         self.val = x
         self.left = None
         self.right = None
         self.root = 1
-
-    # def preOrder(root):
-    #     if root:
-    #         size = 50
-    #         if root == 1:
-    #             x = 1 * 0.8 * pow(2, 1) * size
-    #             y = 0
-    #         scene.addEllipse(x, y, size, size)
-    #         text = scene.addText(str(node.key))
-    #         text.setPos(x + (10 * scale), y + (10 * scale))
-    #         print(root.val)
-    #         if root.left:
-    #             print("Draw Left Here")
-    #             TreeNode.preOrder(root.left)
-    #         if root.right:
-    #             print("Draw Right Here")
-    #             TreeNode.preOrder(root.right)
 
 
 class Codec:
@@ -100,38 +84,19 @@ class VisualizeBinTreeFromJson(QDialog):
         fileName, _ = QFileDialog.getOpenFileName(self, 'Open a file', '',
                                                   'Text Files (*.json)', options=options)
         if fileName:
-            self.import_to_db(fileName)
+            self.export_from_json(fileName)
 
-    # def import_to_db(self, filename):
-    #     self.conn = sqlite3.connect("database.db")
-    #     self.c = self.conn.cursor()
-    #     self.c.execute("DROP TABLE IF EXISTS employee")
-    #     self.c.execute("""
-    #                 CREATE TABLE IF NOT EXISTS employee(roll INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT,branch TEXT,
-    #                 sem TEXT,mobile INTEGER,address TEXT)""")
-
-    # table_name = "employee"
-    # orders = pandas.read_csv(filename)  # load to DataFrame
-    # orders.to_sql(table_name, self.conn, if_exists='append', index=False)
-
-    def import_to_db(self, filename):
-        # Диалог выходит за область видимости
-        # TODO: возвращать ссылку на диалог
-        # ----------------------------------
+    def export_from_json(self, filename):
         global dialog1
 
-        dialog1 = myWindow()
+        file = open(filename, 'r')
+        unhandled_data_from_json = file.read()
+
+        dialog1 = myWindow(unhandled_data_from_json)
         dialog1.show()
 
 
-class Node:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
-        self.root = 1
-
-class BinaryTree1:
+class BinaryTree:
 
     def __init__(self, root=None):
         if root is None:
@@ -145,164 +110,38 @@ class BinaryTree1:
             self.lvlCount = 0
             self.maxLCount = 0
 
-    # def AddNode(self, keyP: int, nodeP=None, currLvl=0):
-    #     if nodeP is None:
-    #         nodeP = self.root
-    #     if self.count == 0:
-    #         self.root = Node(keyP)
-    #     elif keyP < nodeP.key:
-    #         currLvl += 1
-    #         if currLvl > self.lvlCount:
-    #             self.lvlCount = currLvl
-    #         if nodeP.leftCh is None:
-    #             nodeP.leftCh = Node(keyP, nodeP)
-    #         else:
-    #             self.count -= 1
-    #             self.AddNode(keyP, nodeP.leftCh, currLvl)
-    #     else:
-    #         currLvl += 1
-    #         if currLvl > self.lvlCount:
-    #             self.lvlCount = currLvl
-    #         if nodeP.rightCh is None:
-    #             nodeP.rightCh = Node(keyP, nodeP)
-    #         else:
-    #             self.count -= 1
-    #             self.AddNode(keyP, nodeP.rightCh, currLvl)
-    #     self.count += 1
-
     def GenerateTree(self):
-        # for i in range(10):
-        #     self.AddNode(random.randint(0, 1245))
-
         codec = Codec()
 
+        # data = '{"r": {"l": {"l": {"v": 9}, "v": 2}, "v": 7}, "l": {"l": {"l": {"v": -1}, "v": 3}, "v": 4}, "v": 5}'
         data = '{"v": 1, "r": {"v": 2, "l": {"v": 3}}, "l": {"v":5}}'
         des = codec.deserialize(data)
-        # здесь я получаю дерево
-        print(des.right.left.val)
 
-        ser = codec.serialize(des)
-        print(ser)
+        # #### serialize for debug
+        # ser = codec.serialize(des)
+        # print(ser)
+        # ####
+
         return des
 
-    # def SearchNode(self, keyP: int, nodeP):
-    #     if nodeP is None:
-    #         return None
-    #     if nodeP.key == keyP:
-    #         return nodeP
-    #     elif keyP < nodeP.key:
-    #         if nodeP.leftCh is None:
-    #             return None
-    #         else:
-    #             return self.SearchNode(keyP, nodeP.leftCh)
-    #     else:
-    #         if nodeP.rightCh is None:
-    #             return None
-    #         else:
-    #             return self.SearchNode(keyP, nodeP.rightCh)
-
-    # def DeleteNode(self, keyP):
-    #     self.count-=1
-    #     searchedElem = self.SearchNode(keyP, self.root)
-    #     if searchedElem is None:
-    #         return
-    #     else:
-    #         if searchedElem.leftCh is None and searchedElem.rightCh is None:
-    #             if searchedElem == self.root:
-    #                 self.root = None
-    #             else:
-    #                 if searchedElem.parent.leftCh == searchedElem:
-    #                     searchedElem.parent.leftCh = None
-    #                 else:
-    #                     searchedElem.parent.rightCh = None
-    #         else:
-    #             if searchedElem.leftCh is None:
-    #                 searchedElem.rightCh.parent = searchedElem.parent
-    #                 if searchedElem == self.root:
-    #                     self.root = searchedElem.rightCh
-    #                 else:
-    #                     if searchedElem.parent.leftCh == searchedElem:
-    #                         searchedElem.parent.leftCh = searchedElem.rightCh
-    #                     else:
-    #                         searchedElem.parent.rightCh = searchedElem.rightCh
-    #             else:
-    #                 if searchedElem.rightCh is None:
-    #                     searchedElem.leftCh.parent = searchedElem.parent
-    #                     if searchedElem == self.root:
-    #                         self.root = searchedElem.leftCh
-    #                     else:
-    #                         if searchedElem.parent.leftCh == searchedElem:
-    #                             searchedElem.parent.leftCh = searchedElem.leftCh
-    #                         else:
-    #                             searchedElem.parent.rightCh = searchedElem.leftCh
-    #                 else:
-    #                     self.count+=1
-    #                     nodeT = searchedElem.rightCh
-    #                     while nodeT.leftCh is not None:
-    #                         nodeT = nodeT.leftCh
-    #                     keyT = nodeT.key
-    #                     self.DeleteNode(nodeT.key)
-    #                     searchedElem.key = keyT
-    #     self.CountLvl()
-
-    # def CountLvl(self, nodeP=None, lCount=0):
-    #     if nodeP is None:
-    #         if self.root is None:
-    #             return
-    #         else:
-    #             self.maxLCount = 0
-    #             nodeP = self.root
-    #     if nodeP.leftCh is not None:
-    #         lCount +=1
-    #         if lCount>self.maxLCount:
-    #             self.maxLCount = lCount
-    #         self.CountLvl(nodeP.leftCh, lCount)
-    #         lCount -= 1
-    #     if nodeP.rightCh is not None:
-    #         lCount += 1
-    #         if lCount>self.maxLCount:
-    #             self.maxLCount = lCount
-    #         self.CountLvl(nodeP.rightCh, lCount)
-    #         lCount -= 1
-    #     self.lvlCount = self.maxLCount
-    #
-    # def Clear(self):
-    #     if self.root is not None:
-    #         self.DeleteNode(self.root.key)
-    #         self.Clear()
-
-
-    def paint(self, scene, scale, des , x=0, y=0, currLvl=0, currNum=1):
-        ###########################################3
-
+    def paint(self, scene, scale, des, x=0, y=0, currLvl=0, currNum=1):
         if des is None:
             return
         size = 50
         if des.val == des.root:
-            x = currNum*scale*pow(2, self.lvlCount) * size
+            x = currNum * scale * pow(2, self.lvlCount) * size
             y = 0
         scene.addEllipse(x, y, size, size)
         text = scene.addText(str(des.val))
-        text.setPos(x+(10*scale), y+(10*scale))
+        text.setPos(x + (10 * scale), y + (10 * scale))
         nextNumL = currNum * 2 - 1
         nextNumR = currNum * 2
-        nextXL = x-pow(2, self.lvlCount-currLvl)*size*0.50
-        nextXR = x+pow(2, self.lvlCount-currLvl)*size*0.50
+        nextXL = x - pow(2, self.lvlCount - currLvl) * size * 3
+        nextXR = x + pow(2, self.lvlCount - currLvl) * size * 3
         nextY = y + 150
         if des.left is not None:
-            scene.addLine(x + size*0.5, y + size, nextXL + size*0.5, nextY)
+            scene.addLine(x + size * 0.5, y + size, nextXL + size * 0.5, nextY)
             self.paint(scene, scale, des.left, nextXL, nextY, currLvl + 1, currNum * 2 - 1)
         if des.right is not None:
-            scene.addLine(x + size*0.5, y + size, nextXR + size*0.5, nextY)
+            scene.addLine(x + size * 0.5, y + size, nextXR + size * 0.5, nextY)
             self.paint(scene, scale, des.right, nextXR, nextY, currLvl + 1, currNum * 2)
-
-    def print(self, lvl, node=None,  currLvl=0):
-        if node is None:
-            node = self.root
-        if currLvl == lvl:
-            print(node.key, end=" ")
-        else:
-            if node.leftCh is not None:
-                self.print(lvl, node.leftCh, currLvl+1)
-            if node.rightCh is not None:
-                self.print(lvl, node.rightCh, currLvl+1)
--- BinaryTreeWindow.py
+++ BinaryTreeWindow.py
@@ -10,35 +10,21 @@ import pandas
 
 class myWindow(QMainWindow):
 
-    def __init__(self):
+    def __init__(self, unhandled_data_from_json):
+
         super(myWindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.tree = BinaryTree.BinaryTree1()
+        # генерация дерева
+        self.tree = BinaryTree.BinaryTree()
+        # присваивание необработанных данных из json дереву
+        self.unhandled_data_from_json = unhandled_data_from_json
 
         self.scene = QtWidgets.QGraphicsScene()
         self.ui.graphicsView.setScene(self.scene)
         self.scale = 0.8
         self.ui.graphicsView.scale(self.scale, self.scale)
-        self.ui.pushButton.clicked.connect(self.AddEl)
-        self.ui.pushButton_2.clicked.connect(self.DelEl)
-        self.ui.pushButton_4.clicked.connect(self.Clear)
         self.ui.pushButton_5.clicked.connect(self.Generate)
-
-    def AddEl(self):
-        self.tree.AddNode(int(self.ui.spinBox.text()))
-        self.scene.clear()
-        self.PaintT()
-
-    def DelEl(self):
-        self.tree.DeleteNode(int(self.ui.spinBox_2.text()))
-        self.scene.clear()
-        self.PaintT()
-
-    def Clear(self):
-        self.scene.clear()
-        self.tree.Clear()
-        self.PaintT()
 
     def Generate(self):
         self.scene.clear()
@@ -46,17 +32,10 @@ class myWindow(QMainWindow):
         self.PaintT()
 
     def PaintT(self):
-        self.ui.label_5.setText(str(self.tree.count))
-        self.ui.label_7.setText(str(self.tree.lvlCount))
-        # Передеать в paint des данные
-        ## ЧТОБЫ ВТОРОЙ РАЗ НЕ ПЕРЕСОЗДАВАЛО ОБЪЕКТ
         codec = BinaryTree.Codec()
 
-        data = '{"v": 1, "r": {"v": 2, "l": {"v": 3}}, "l": {"v":5}}'
+        data = self.unhandled_data_from_json
         des = codec.deserialize(data)
-        # здесь я получаю дерево
-        print(des.right.left.val)
-        ############################################
         self.tree.paint(self.scene, self.scale, des)
 
 
@@ -68,64 +47,37 @@ class Ui_MainWindow(object):
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.graphicsView = QtWidgets.QGraphicsView(self.centralwidget)
-        self.graphicsView.setGeometry(QtCore.QRect(10, 10, 801, 661))
+        self.graphicsView.setGeometry(QtCore.QRect(10, 10, 600, 661))
         self.graphicsView.setObjectName("graphicsView")
+
         self.groupBox = QtWidgets.QGroupBox(self.centralwidget)
-        self.groupBox.setGeometry(QtCore.QRect(820, 20, 241, 91))
+        self.groupBox.setGeometry(QtCore.QRect(615, 20, 325, 130))
         self.groupBox.setObjectName("groupBox")
-        self.spinBox = QtWidgets.QSpinBox(self.groupBox)
-        self.spinBox.setGeometry(QtCore.QRect(140, 30, 71, 21))
-        self.spinBox.setMinimum(-10000)
-        self.spinBox.setMaximum(10000)
-        self.spinBox.setProperty("value", 0)
-        self.spinBox.setObjectName("spinBox")
         self.label = QtWidgets.QLabel(self.groupBox)
-        self.label.setGeometry(QtCore.QRect(10, 30, 121, 21))
+        self.label.setGeometry(QtCore.QRect(10, 5, 325, 130))
         self.label.setObjectName("label")
-        self.pushButton = QtWidgets.QPushButton(self.groupBox)
-        self.pushButton.setGeometry(QtCore.QRect(60, 60, 121, 23))
-        self.pushButton.setObjectName("pushButton")
+
         self.groupBox_2 = QtWidgets.QGroupBox(self.centralwidget)
-        self.groupBox_2.setGeometry(QtCore.QRect(820, 120, 241, 91))
+        self.groupBox_2.setGeometry(QtCore.QRect(615, 160, 325, 110))
         self.groupBox_2.setObjectName("groupBox_2")
-        self.spinBox_2 = QtWidgets.QSpinBox(self.groupBox_2)
-        self.spinBox_2.setGeometry(QtCore.QRect(140, 30, 71, 21))
-        self.spinBox_2.setMinimum(-10000)
-        self.spinBox_2.setMaximum(10000)
-        self.spinBox_2.setObjectName("spinBox_2")
         self.label_2 = QtWidgets.QLabel(self.groupBox_2)
-        self.label_2.setGeometry(QtCore.QRect(10, 30, 121, 21))
+        self.label_2.setGeometry(QtCore.QRect(10, 10, 325, 110))
         self.label_2.setObjectName("label_2")
-        self.pushButton_2 = QtWidgets.QPushButton(self.groupBox_2)
-        self.pushButton_2.setGeometry(QtCore.QRect(60, 60, 121, 23))
-        self.pushButton_2.setObjectName("pushButton_2")
-        self.groupBox_4 = QtWidgets.QGroupBox(self.centralwidget)
-        self.groupBox_4.setGeometry(QtCore.QRect(820, 360, 241, 91))
-        self.groupBox_4.setObjectName("groupBox_4")
-        self.label_4 = QtWidgets.QLabel(self.groupBox_4)
-        self.label_4.setGeometry(QtCore.QRect(10, 20, 61, 16))
-        self.label_4.setObjectName("label_4")
-        self.label_5 = QtWidgets.QLabel(self.groupBox_4)
-        self.label_5.setGeometry(QtCore.QRect(80, 20, 47, 16))
-        self.label_5.setObjectName("label_5")
-        self.label_6 = QtWidgets.QLabel(self.groupBox_4)
-        self.label_6.setGeometry(QtCore.QRect(10, 50, 61, 16))
-        self.label_6.setObjectName("label_6")
-        self.label_7 = QtWidgets.QLabel(self.groupBox_4)
-        self.label_7.setGeometry(QtCore.QRect(80, 50, 47, 16))
-        self.label_7.setObjectName("label_7")
+
         self.groupBox_5 = QtWidgets.QGroupBox(self.centralwidget)
-        self.groupBox_5.setGeometry(QtCore.QRect(820, 220, 241, 61))
+        self.groupBox_5.setGeometry(QtCore.QRect(615, 280, 325, 40))
         self.groupBox_5.setObjectName("groupBox_5")
-        self.pushButton_4 = QtWidgets.QPushButton(self.groupBox_5)
-        self.pushButton_4.setGeometry(QtCore.QRect(60, 30, 121, 23))
-        self.pushButton_4.setObjectName("pushButton_4")
+        self.label_3 = QtWidgets.QLabel(self.groupBox_5)
+        self.label_3.setGeometry(QtCore.QRect(10, 10, 325, 40))
+        self.label_3.setObjectName("label_3")
+
         self.groupBox_6 = QtWidgets.QGroupBox(self.centralwidget)
-        self.groupBox_6.setGeometry(QtCore.QRect(820, 290, 241, 61))
+        self.groupBox_6.setGeometry(QtCore.QRect(615, 340, 325, 65))
         self.groupBox_6.setObjectName("groupBox_6")
         self.pushButton_5 = QtWidgets.QPushButton(self.groupBox_6)
-        self.pushButton_5.setGeometry(QtCore.QRect(60, 30, 121, 23))
+        self.pushButton_5.setGeometry(QtCore.QRect(120, 30, 121, 23))
         self.pushButton_5.setObjectName("pushButton_5")
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 1080, 21))
@@ -133,7 +85,6 @@ class Ui_MainWindow(object):
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
-        # MainWindow.setMainWindowStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
@@ -141,18 +92,24 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.groupBox.setTitle(_translate("MainWindow", "Добавить элемент"))
-        self.label.setText(_translate("MainWindow", "Введите значение:"))
-        self.pushButton.setText(_translate("MainWindow", "Добавить элемент"))
-        self.groupBox_2.setTitle(_translate("MainWindow", "Удалить элемент"))
-        self.label_2.setText(_translate("MainWindow", "Введите значение:"))
-        self.pushButton_2.setText(_translate("MainWindow", "Удалить элемент"))
-        self.groupBox_4.setTitle(_translate("MainWindow", "Количество элементов и высота дерева"))
-        self.label_4.setText(_translate("MainWindow", "Элементов:"))
-        self.label_5.setText(_translate("MainWindow", "0"))
-        self.label_6.setText(_translate("MainWindow", "Высота:"))
-        self.label_7.setText(_translate("MainWindow", "0"))
-        self.groupBox_5.setTitle(_translate("MainWindow", "Очистить дерево"))
-        self.pushButton_4.setText(_translate("MainWindow", "Очистить"))
-        self.groupBox_6.setTitle(_translate("MainWindow", "Добавить в дерево 10 элементов"))
-        self.pushButton_5.setText(_translate("MainWindow", "Добавить"))
+
+        self.groupBox.setTitle(_translate("MainWindow", "Описание визуализации"))
+        self.label.setText(_translate("MainWindow", """Отображение на графике структуры
+данных в виде бинарного дерева и связи между
+этими данными. Данные о структуре данных
+хранятся в формате json, которые подаются
+на вход"""))
+
+        self.groupBox_2.setTitle(_translate("MainWindow", "Описание входных данных"))
+        self.label_2.setText(_translate("MainWindow", """Входные данные представляют
+собой одностроковый json файл бинарного
+дерева, где
+-- v - корневой элемент
+-- r - правый элемент корня
+-- l - левый элемент корня"""))
+
+        self.groupBox_5.setTitle(_translate("MainWindow", "Пример входных данных"))
+        self.label_3.setText(_translate("MainWindow", '{"v": 1, "r": {"v": 2, "l": {"v": 3}}, "l": {"v":5}}'))
+
+        self.groupBox_6.setTitle(_translate("MainWindow", "Сгенерировать дерево"))
+        self.pushButton_5.setText(_translate("MainWindow", "Начать"))
